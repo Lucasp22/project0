a9af9d3b389766ea89661f6bc5365ece76c0d712
Lucas Padua <lucas.p2222@gmail.ciom>
2018-06-26 09:31:10 +1000

    Winner Fx

--- js/main.js
+++ js/main.js
@@ -19,9 +19,32 @@ const playO = function (index){
   board[index] = "O";
 }
 /////////Check for a winner Fx////
+const winnerFx = function (){
+  if (board[ 0 ] === board[ 1 ] && board[ 1 ] === board[ 2 ]) {
+  console.log(`${ board[ 0 ] } winner`);
 
+}else if (board[ 3 ] === board[ 4 ] && board[ 4 ] === board[ 5 ]) {
+  console.log(`${ board[ 3 ] } winner`);
 
+}else if (board[ 6 ] === board[ 7 ] && board[ 7 ] === board[ 8 ]) {
+  console.log(`${ board[ 6 ] } winner`);
 
+}else if (board[ 0 ] === board[ 3 ] && board[ 3 ] === board[ 6 ]) {
+  console.log(`${ board[ 0 ] } winner`);
+
+}else if (board[ 1 ] === board[ 4 ] && board[ 4 ] === board[ 7 ]) {
+  console.log(`${ board[ 1 ] } winner`);
+
+}else if (board[ 2 ] === board[ 5 ] && board[ 5 ] === board[ 8 ]) {
+  console.log(`${ board[ 2 ] } winner`);
+
+}else if (board[ 6 ] === board[ 4 ] && board[ 4 ] === board[ 2 ]) {
+  console.log(`${ board[ 6 ] } winner`);
+
+}else if (board[ 0 ] === board[ 4 ] && board[ 4 ] === board[ 8 ]) {
+  console.log(`${ board[ 0 ] } winner`);
+}
+}
 //////player turn function
 const playTurn = function(index){
   if (playerOneIsNext === true){
