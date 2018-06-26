2ef7b2e0ce66e9c2481d21a9bebfd670617e2223
Lucas Padua <lucas.p2222@gmail.ciom>
2018-06-26 11:41:10 +1000

    Game Logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 console.log(`Hello Lucas`)
 
 
-const board = ["", "", "",
-              "", "", "",
-              "", "", ""];
+const board = [" ", " ", " ",
+              " ", " ", " ",
+              " ", " ", " "];
 
 ////Global variables
 
 let playerOneIsNext = true;
+let numberOfPlay = 9;
 
 /////playX turn
 const playX = function(index){
@@ -20,38 +21,57 @@ const playO = function (index){
 }
 /////////Check for a winner Fx////
 const winnerFx = function (){
-  if (board[ 0 ] === board[ 1 ] && board[ 1 ] === board[ 2 ]) {
+  if (board[ 0 ] === board[ 1 ] && board[ 1 ] === board[ 2 ] && board[ 0 ] != " ") {
   console.log(`${ board[ 0 ] } winner`);
+  return true;
 
-}else if (board[ 3 ] === board[ 4 ] && board[ 4 ] === board[ 5 ]) {
+}else if (board[ 3 ] === board[ 4 ] && board[ 4 ] === board[ 5 ] && board[ 3 ] != " ") {
   console.log(`${ board[ 3 ] } winner`);
+  return true;
 
-}else if (board[ 6 ] === board[ 7 ] && board[ 7 ] === board[ 8 ]) {
+}else if (board[ 6 ] === board[ 7 ] && board[ 7 ] === board[ 8 ] && board[ 6 ] != " ") {
   console.log(`${ board[ 6 ] } winner`);
+  return true;
 
-}else if (board[ 0 ] === board[ 3 ] && board[ 3 ] === board[ 6 ]) {
+}else if (board[ 0 ] === board[ 3 ] && board[ 3 ] === board[ 6 ] && board[ 0 ] != " ") {
   console.log(`${ board[ 0 ] } winner`);
+  return true;
 
-}else if (board[ 1 ] === board[ 4 ] && board[ 4 ] === board[ 7 ]) {
+}else if (board[ 1 ] === board[ 4 ] && board[ 4 ] === board[ 7 ] && board[ 1 ] != " ") {
   console.log(`${ board[ 1 ] } winner`);
+  return true;
 
-}else if (board[ 2 ] === board[ 5 ] && board[ 5 ] === board[ 8 ]) {
+}else if (board[ 2 ] === board[ 5 ] && board[ 5 ] === board[ 8 ] && board[ 2 ] != " ") {
   console.log(`${ board[ 2 ] } winner`);
+  return true;
 
-}else if (board[ 6 ] === board[ 4 ] && board[ 4 ] === board[ 2 ]) {
+}else if (board[ 6 ] === board[ 4 ] && board[ 4 ] === board[ 2 ] && board[ 6 ] != " ") {
   console.log(`${ board[ 6 ] } winner`);
+  return true;
 
-}else if (board[ 0 ] === board[ 4 ] && board[ 4 ] === board[ 8 ]) {
+}else if (board[ 0 ] === board[ 4 ] &&  board[ 4 ] === board[ 8 ] && board[ 0 ] != " "){
   console.log(`${ board[ 0 ] } winner`);
+  return true;
+
 }
 }
 //////player turn function
 const playTurn = function(index){
   if (playerOneIsNext === true){
     playX(index);
+    numberOfPlay += 1 ///// counting turns
     playerOneIsNext = false;
   } else{
     playO(index);
+    numberOfPlay += 1 //// counting turns
     playerOneIsNext = true;
+  }
+  winnerFx()
+  draw()
+  }
+//////////// draw function///////
+const draw = function(){
+  if ( (numberOfPlay >= 9) && (winnerFx() !== true) ){
+    console.log("Draw");
   }
 }
