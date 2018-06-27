594e321050f76a4bb674a0c8f61b4a64ca6e0496
Lucas Padua <lucas.p2222@gmail.ciom>
2018-06-27 14:56:51 +1000

    Logic and HTML AND CSS

--- js/main.js
+++ js/main.js
@@ -34,30 +34,44 @@ const winnerFx = function (){
 
 }else if (board[ 3 ] === board[ 4 ] && board[ 4 ] === board[ 5 ] && board[ 3 ] != " ") {
   $('.hidden').show();
+  $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
+
   return true;
 
 }else if (board[ 6 ] === board[ 7 ] && board[ 7 ] === board[ 8 ] && board[ 6 ] != " ") {
   $('.hidden').show();
+  $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
+
   return true;
 
 }else if (board[ 0 ] === board[ 3 ] && board[ 3 ] === board[ 6 ] && board[ 0 ] != " ") {
   $('.hidden').show();
+  $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
+
   return true;
 
 }else if (board[ 1 ] === board[ 4 ] && board[ 4 ] === board[ 7 ] && board[ 1 ] != " ") {
   $('.hidden').show();
+  $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
+
+
   return true;
 
 }else if (board[ 2 ] === board[ 5 ] && board[ 5 ] === board[ 8 ] && board[ 2 ] != " ") {
   $('.hidden').show();
+  $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
+
   return true;
 
 }else if (board[ 6 ] === board[ 4 ] && board[ 4 ] === board[ 2 ] && board[ 6 ] != " ") {
   $('.hidden').show();
+  $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
+
   return true;
 
 }else if (board[ 0 ] === board[ 4 ] &&  board[ 4 ] === board[ 8 ] && board[ 0 ] != " "){
   $('.hidden').show();
+  $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
 
   return true;
 
