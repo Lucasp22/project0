8a797117ad75f6b578fc8807eeb6e839def24182
Lucas Padua <lucas.p2222@gmail.ciom>
2018-06-27 15:52:02 +1000

    Logic and HTML AND CSS

--- js/main.js
+++ js/main.js
@@ -7,15 +7,15 @@ let numberOfPlay = 0;
 let boxId = 0;
 //////////////PLAYING/////
 const updateBoard = function() {
-  $('#0').html(board[0]);
-  $('#1').html(board[1]);
-  $('#2').html(board[2]);
-  $('#3').html(board[3]);
-  $('#4').html(board[4]);
-  $('#5').html(board[5]);
-  $('#6').html(board[6]);
-  $('#7').html(board[7]);
-  $('#8').html(board[8]);
+  $('#0').text(board[0]);
+  $('#1').text(board[1]);
+  $('#2').text(board[2]);
+  $('#3').text(board[3]);
+  $('#4').text(board[4]);
+  $('#5').text(board[5]);
+  $('#6').text(board[6]);
+  $('#7').text(board[7]);
+  $('#8').text(board[8]);
 };
 /////playX turn
 const playX = function(index){
@@ -35,46 +35,37 @@ const winnerFx = function (){
 }else if (board[ 3 ] === board[ 4 ] && board[ 4 ] === board[ 5 ] && board[ 3 ] != " ") {
   $('.hidden').show();
   $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-
   return true;
 
 }else if (board[ 6 ] === board[ 7 ] && board[ 7 ] === board[ 8 ] && board[ 6 ] != " ") {
   $('.hidden').show();
   $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-
   return true;
 
 }else if (board[ 0 ] === board[ 3 ] && board[ 3 ] === board[ 6 ] && board[ 0 ] != " ") {
   $('.hidden').show();
   $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-
   return true;
 
 }else if (board[ 1 ] === board[ 4 ] && board[ 4 ] === board[ 7 ] && board[ 1 ] != " ") {
   $('.hidden').show();
   $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-
-
   return true;
 
 }else if (board[ 2 ] === board[ 5 ] && board[ 5 ] === board[ 8 ] && board[ 2 ] != " ") {
   $('.hidden').show();
   $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-
   return true;
 
 }else if (board[ 6 ] === board[ 4 ] && board[ 4 ] === board[ 2 ] && board[ 6 ] != " ") {
   $('.hidden').show();
   $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-
   return true;
 
 }else if (board[ 0 ] === board[ 4 ] &&  board[ 4 ] === board[ 8 ] && board[ 0 ] != " "){
   $('.hidden').show();
   $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-
   return true;
-
 }
 }
 //////player turn function
@@ -97,19 +88,24 @@ const playTurn = function(index){
 ////////////resetFx/////
 
 const resetFx = function (){
-  if((winnerFx() === true) || (draw() === true) ){
-  }
+  // if((winnerFx() === true) || (draw() === true) ){
+  // }
     for (var i = 0; i < board.length; i++) {
-     board[i] = " "
+     board[i] = ":-)"
   }
-  updateBoard()
+  numberOfPlay = 0;
+  updateBoard();
+  for (var i = 0; i < board.length; i++) {
+   board[i] = " "
+  }
 }
 
 
 //////////// draw function///////
 const draw = function(){
   if ( (numberOfPlay >= 9) && (winnerFx() !== true) ){
-    $('.hidden').removeClass('hidden');
+    $('.hidden').show();
+    $('#notification').html(` is the DRAW!!!!`)
     console.log("Draw");
     return true;
   }
@@ -125,7 +121,5 @@ $('#Reset').on('click', function() {
   resetFx() // reset the board and screen
   $('.hidden').hide();
 })
-
-
 
 });////////document ready//////
