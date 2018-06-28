bd1a9dc0e04f46deffed4255e68b26db6fb06eb0
Lucas Padua <lucas.p2222@gmail.ciom>
2018-06-28 16:47:08 +1000

    TEST

--- ai/js/main.js
+++ ai/js/main.js
@@ -1,156 +1,24 @@
-console.log("heloo");
+console.log(`Hello Lucas!!!`)
 
-const board = [" ", " ", " ",
-                " ", " ", " ",
+
+const board = ["X", "X", " ",
+                "O", "X", "X",
                 " ", " ", " "
 ];
-////Global variables
-let playerOneIsNext = true;
-let numberOfPlay = 0;
-let boxId = 0;
-//////////////PLAYING/////
-const updateBoard = function() {
-  $('#0').text(board[0]);
-  $('#1').text(board[1]);
-  $('#2').text(board[2]);
-  $('#3').text(board[3]);
-  $('#4').text(board[4]);
-  $('#5').text(board[5]);
-  $('#6').text(board[6]);
-  $('#7').text(board[7]);
-  $('#8').text(board[8]);
-};
-/////playX turn
-const playX = function(index) {
-  board[index] = "X";
-}
-///// playO function
 const playO = function(index) {
   board[index] = "O";
 }
-/////////Check for a winner Fx////
-const winnerFx = function() {
-  if (board[0] === board[1] && board[1] === board[2] && board[0] != " ") {
-    $('.hidden').show();
-    $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-    return true;
 
-  } else if (board[3] === board[4] && board[4] === board[5] && board[3] != " ") {
-    $('.hidden').show();
-    $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-    return true;
-
-  } else if (board[6] === board[7] && board[7] === board[8] && board[6] != " ") {
-    $('.hidden').show();
-    $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-    return true;
-
-  } else if (board[0] === board[3] && board[3] === board[6] && board[0] != " ") {
-    $('.hidden').show();
-    $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-    return true;
-
-  } else if (board[1] === board[4] && board[4] === board[7] && board[1] != " ") {
-    $('.hidden').show();
-    $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-    return true;
-
-  } else if (board[2] === board[5] && board[5] === board[8] && board[2] != " ") {
-    $('.hidden').show();
-    $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-    return true;
-
-  } else if (board[6] === board[4] && board[4] === board[2] && board[6] != " ") {
-    $('.hidden').show();
-    $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-    return true;
-
-  } else if (board[0] === board[4] && board[4] === board[8] && board[0] != " ") {
-    $('.hidden').show();
-    $('#notification').html(`${ board[ 0 ] } is the winner!!!!`)
-    return true;
-  }
-}
 ///////////Atac or Defence
 //
 const atDef = function(){
   if (atacFx() === true) {
     if (defFx()=== true) {
-      randyFx()
-      // setTimeout(function() { randyFx(); }, 1000);
+      randomFx()
     }
 
   }
-}
-
-//////player turn function
-const playTurn = function(index) {
-  if (board[index] === " ") {
-    if (playerOneIsNext === true) {
-      // player turn
-      playX(index);
-      numberOfPlay += 1 ///// counting turns
-      playerOneIsNext = false;
-      // ai turn
-      setTimeout(function () {
-        atDef();
-        winnerFx()
-        draw()
-        updateBoard()
-      }, 1000)
-      numberOfPlay += 1 //// counting turns
-      playerOneIsNext = true;
-    }
-    winnerFx()
-    draw()
-    updateBoard()
-  }
-}
-////////////randyFX////////
-const randyFx = function() {
-  if (board[4] === " " ) {
-    playO(4)
-    return
-  }
-  if (board[0] === " " ) {
-    playO(0)
-    return
-  }
-  if (board[1] === " " ) {
-    playO(1)
-    return
-
-  }
-  if (board[2] === " " ) {
-    playO(2)
-    return
-
-  }
-  if (board[5] === " " ) {
-    playO(5)
-    return
-
-  }
-  if (board[8] === " " ) {
-    playO(8)
-    return
-
-  }
-  if (board[7] === " " ) {
-    playO(7)
-    return
-
-  }
-  if (board[6] === " " ) {
-    playO(6)
-    return
-
-  }
-  if (board[3] === " " ) {
-    playO(3)
-    return
-  }
-}
+};
 ////////////atacFx
 const atacFx = function() {
   if (board[0] === "O" && board[1] === "O" && board[2] === " " ) {
@@ -251,6 +119,7 @@ const atacFx = function() {
   }
   return true;
 }
+
 // /////////////defFx///////////
 const defFx = function() {
   if (board[0] === "X" && board[1] === "X" && board[2] === " " ) {
@@ -351,42 +220,12 @@ const defFx = function() {
   }
   return true;
 }
-
-////////////resetFx/////
-
-const resetFx = function() {
-  // if((winnerFx() === true) || (draw() === true) ){
-  // }
-  for (var i = 0; i < board.length; i++) {
-    board[i] = ":-)"
+//////////////////
+const randomFx = function() {
+  if (board[0] === " " ) {
+    playO(0)
   }
-  numberOfPlay = 0;
-  updateBoard();
-  for (var i = 0; i < board.length; i++) {
-    board[i] = " "
+  if (board[0] === " " ) {
+    playO(0)
   }
-}
-
-
-//////////// draw function///////
-const draw = function() {
-  if ((numberOfPlay >= 9) && (winnerFx() !== true)) {
-    $('.hidden').show();
-    $('#notification').html(` is the DRAW!!!!`)
-    console.log("Draw");
-    return true;
-  }
-}
-$(document).ready(function() { /////////document ready
-  ///////////fx target////
-  $('.box').click(function() {
-    boxId = event.target.id; /// returns a number
-    playTurn(boxId);
-  });
-  ///////listinner fx for button
-  $('#Reset').on('click', function() {
-    resetFx() // reset the board and screen
-    $('.hidden').hide();
-  })
-
-}); ////////document ready//////
+}  
